fde293803c2f33ee3af68be04534b78093ac8c0b
med03ali <medali24122003@gmail.com>
2025-04-16 10:55:44 +0100

    miloudii2

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+package Desktop.DevopsAtelier;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println("5 + 3 = " + calc.add(5, 3));
+    }
+}
