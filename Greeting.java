b78fdf2f4733cbfc23a49e13ade8ad151dadae0f
med03ali <medali24122003@gmail.com>
2025-04-16 10:58:12 +0100

    miloudii3

---
+++ Greeting.java
@@ -0,0 +1,11 @@
+package Desktop.DevopsAtelier;
+
+public class Greeting {
+    public static void greet(String name) {
+        System.out.println("Salut " + name + ", bienvenue dans le projet !");
+    }
+
+    public static void main(String[] args) {
+        greet("Aliel");
+    }
+}
